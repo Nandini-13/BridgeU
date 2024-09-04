98d7fba24294d1bfc7675a05b13e8ff2d19705fa
Nandini-13 <nandinigangadharan13@gmail.com>
2024-09-04 19:00:59 +0530

    New edit

---
+++ script.js
@@ -0,0 +1,7 @@
+// Responsive Navbar Toggle
+const burger = document.querySelector('.burger');
+const navLinks = document.querySelector('.nav-links');
+
+burger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
